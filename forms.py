5d6f50eddb400a961e3660e61b63ed16a37aecaf
u2260774 <126501906+u2260774@users.noreply.github.com>
2024-02-19 20:26:51 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import InputRequired
 class InputForm(FlaskForm):
     resp_type = SelectField('What is the respiration type?', choices=[('', 'Please select'), ('2', 'Oxygen'), ('0', 'Air')], validators=[InputRequired()])
     conc = SelectField('Is the patient concious?', choices=[('', 'Please select'), ('0', 'Aware'), ('3', 'Unconcious')], validators=[InputRequired()])
-    resp_rate = IntegerField('Respirate Rate', validators=[InputRequired()])
+    resp_rate = IntegerField('Respiration Rate', validators=[InputRequired()])
     time_since = IntegerField('How many hours since last meal', validators=[InputRequired()])
     spo2 = IntegerField('Oxygen Saturation (0-100)', validators=[InputRequired()])
     temp = FloatField("Patient's Body Temperature", validators=[InputRequired()])
